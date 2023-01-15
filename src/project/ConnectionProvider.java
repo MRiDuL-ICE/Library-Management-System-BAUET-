2e27aa83cb05dc329cad8a906313649f97657d4d
Mridul03-ice <abdulwahab22400@gmail.com>
2023-01-15 18:00:54 +0600

    Files upload

---
+++ src/project/ConnectionProvider.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+import java.sql.*;
+
+/**
+ *
+ * @author Abdul wahab Mridul
+ */
+public class ConnectionProvider {
+    public static Connection getCon()
+    {
+        try 
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/lms","root","abdulwahab22400@");
+            return con;
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
